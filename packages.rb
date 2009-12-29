371d9d581c8924eaddd3f552fe31cb11a7d8a9a9
David Goodlad <david@goodlad.ca>
2009-12-29 20:37:08 +1100

    Fix erlang packages

--- packages.rb
+++ packages.rb
@@ -1,6 +1,10 @@
 pkg 'automake', :for => :linux
-pkg 'erlang-dev', :for => :linux
-pkg 'erlang-nox', :for => :linux
+pkg 'erlang-dev', :for => :linux do
+  provides 'erlc'
+end
+pkg 'erlang-nox', :for => :linux do
+  provides 'erl'
+end
 pkg 'libicu-dev', :for => :linux
 pkg 'help2man', :for => :linux
 pkg 'libtool', :for => :linux
