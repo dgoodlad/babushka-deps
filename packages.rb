0c7ca64b332a649d2ac2197f92562b2547e45682
David Goodlad <david@goodlad.ca>
2009-12-29 20:38:27 +1100

    Fix provides for other pkgs

--- packages.rb
+++ packages.rb
@@ -5,9 +5,15 @@ end
 pkg 'erlang-nox', :for => :linux do
   provides 'erl'
 end
-pkg 'libicu-dev', :for => :linux
+pkg 'libicu-dev', :for => :linux do
+  provides []
+end
 pkg 'help2man', :for => :linux
 pkg 'libtool', :for => :linux
-pkg 'libmozjs-dev', :for => :linux
-pkg 'libcurl4-openssl-dev', :for => :linux
+pkg 'libmozjs-dev', :for => :linux do
+  provides []
+end
+pkg 'libcurl4-openssl-dev', :for => :linux do
+  provides []
+end
 
