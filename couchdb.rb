3f30c7be01aadf8cd82a8769a5ba120f9ae53ec7
David Goodlad <david@goodlad.ca>
2009-12-29 21:12:16 +1100

    Add couchdb startup scripts

--- couchdb.rb
+++ couchdb.rb
@@ -9,3 +9,30 @@ src 'couchdb' do
     shell 'make install', :sudo => true
   end
 end
+
+dep 'couchdb startup script' do
+  requires 'couchdb'
+  met? { shell("rcconf --list").val_for('couchdb') == 'on' }
+  meet :on => :linux do
+    sudo "ln -sf #{prefix}/etc/init.d/couchdb /etc/init.d/couchdb"
+    sudo "ln -sf #{prefix}/etc/default/couchdb /etc/default/couchdb"
+    sudo 'update-rc.d couchdb defaults'
+  end
+end
+
+def couchdb_running?
+  shell "netstat -an | grep -E '^tcp.*[.:]5984 +.*LISTEN'"
+end
+
+dep 'couchdb running' do
+  requires 'webserver configured', 'webserver startup script'
+  met? {
+    returning couchdb_running? do |result|
+      result "There is #{result ? 'something' : 'nothing'} listening on #{result ? result.scan(/[0-9.*]+[.:]5984/).first : 'port 5984'}", :result => result
+    end
+  }
+  meet :on => :linux do
+    sudo '/etc/init.d/couchdb start'
+  end
+end
+
