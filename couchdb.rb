9949a9400d792913042bed04071457b9732d0734
David Goodlad <david@goodlad.ca>
2010-04-19 18:27:06 +1000

    Update packages, and use couchdb 0.11.0 tarball

--- couchdb.rb
+++ couchdb.rb
@@ -1,17 +1,23 @@
 src 'couchdb' do
-  requires 'help2man', 'libtool', 'autoconf', 'automake',
-           'erlang-dev', 'erlang-nox', 'libicu-dev', 'libmozjs-dev', 'libcurl4-openssl-dev'
-  source 'git://git.apache.org/couchdb.git'
-  preconfigure do
-    shell './bootstrap'
-  end
-  install do
-    shell 'make install', :sudo => true
+  requires 'help2man', 'libtool', 'erlang', 'libicu', 'spidermonkey', 'libcurl-dev',
+           'couchdb user'
+  #source 'git://git.apache.org/couchdb.git'
+  source 'http://apache.mirror.aussiehq.net.au/couchdb/0.11.0/apache-couchdb-0.11.0.tar.gz'
+  #preconfigure do
+  #  shell './bootstrap'
+  #end
+  after :on => :linux do
+    # The couchdb user needs to be able to write to logs, pidfiles, and dbs
+    sudo "chown couchdb /usr/local/var/log/couchdb"
+    sudo "chown couchdb /usr/local/var/run/couchdb"
+    sudo "chown couchdb /usr/local/var/lib/couchdb"
+    # ... and the local config
+    sudo "chown couchdb /usr/local/etc/couchdb/local.ini"
   end
 end
 
 dep 'couchdb startup script' do
-  requires 'couchdb', 'couchdb user'
+  requires 'couchdb', 'couchdb user', 'rcconf'
   met? { shell("rcconf --list").val_for('couchdb') == 'on' }
   meet :on => :linux do
     sudo "ln -sf /usr/local/etc/init.d/couchdb /etc/init.d/couchdb"
@@ -21,20 +27,22 @@ dep 'couchdb startup script' do
 end
 
 dep 'couchdb user' do
-  set :username, 'couchdb'
-  set :home_base, '/usr/local/var/run'
+  setup {
+    set :username, 'couchdb'
+    set :home_dir_base, '/usr/local/var/run'
+  }
   requires 'user exists'
 end
 
-def couchdb_running?
-  shell "netstat -an | grep -E '^tcp.*[.:]5984 +.*LISTEN'"
-end
-
 dep 'couchdb running' do
+  helper :couchdb_running? do
+    shell "netstat -an | grep -E '^tcp.*[.:]5984 +.*LISTEN'"
+  end
+
   requires 'couchdb startup script'
   met? {
     returning couchdb_running? do |result|
-      result "There is #{result ? 'something' : 'nothing'} listening on #{result ? result.scan(/[0-9.*]+[.:]5984/).first : 'port 5984'}", :result => result
+      log "There is #{result ? 'something' : 'nothing'} listening on #{result ? result.scan(/[0-9.*]+[.:]5984/).first : 'port 5984'}"
     end
   }
   meet :on => :linux do
--- packages.rb
+++ packages.rb
@@ -1,19 +1,55 @@
-pkg 'automake', :for => :linux
-pkg 'erlang-dev', :for => :linux do
-  provides 'erlc'
+dep 'libicu' do
+  requires {
+    on :osx, 'src libicu'
+    on :linux, 'libicu-dev'
+  }
 end
-pkg 'erlang-nox', :for => :linux do
-  provides 'erl'
-end
-pkg 'libicu-dev', :for => :linux do
-  provides []
-end
-pkg 'help2man', :for => :linux
-pkg 'libtool', :for => :linux
-pkg 'libmozjs-dev', :for => :linux do
-  provides []
-end
-pkg 'libcurl4-openssl-dev', :for => :linux do
+
+pkg 'libicu-dev' do
+  installs { via :apt, 'libicu-dev' }
   provides []
 end
 
+src 'src libicu' do
+  # TODO fill this in!
+end
+
+pkg 'help2man'
+
+pkg 'libcurl-dev' do
+  installs {
+    via :apt, 'libcurl4-openssl-dev'
+  }
+  provides []
+end
+
+dep 'spidermonkey' do
+  requires {
+    on :linux, 'src spidermonkey'
+    on :osx, 'pkg spidermonkey'
+  }
+end
+
+src 'src spidermonkey' do
+  source 'http://ftp.mozilla.org/pub/mozilla.org/js/js-1.8.0-rc1.tar.gz'
+  configure { true } # Noop
+  build { shell "make BUILD_OPT=1 -C js/src -f Makefile.ref" }
+  install { sudo "make BUILD_OPT=1 JS_DIST='#{prefix}' -C js/src -f Makefile.ref export" }
+  provides 'js'
+end
+
+pkg 'pkg spidermonkey' do
+  installs {
+    via :brew, 'spidermonkey'
+    via :apt, 'libxulrunner-dev'
+  }
+  provides 'js'
+end
+
+pkg 'erlang' do
+  installs {
+    via :brew, 'erlang'
+    via :apt, 'erlang-nox', 'erlang-dev'
+  }
+  provides 'erlc', 'erl'
+end
