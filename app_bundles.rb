4b12fd272fb163eca36914fef74fe074eb4ba6d8
David Goodlad <david@goodlad.ca>
2010-07-20 23:06:28 +1000

    Add a few more apps

--- app_bundles.rb
+++ app_bundles.rb
@@ -7,3 +7,19 @@ dep 'SizeUp.app' do
   source 'http://irradiatedsoftware.com/downloads/SizeUp.zip'
 end
 
+dep 'iStat Menus.app' do
+  source 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.03.zip'
+end
+
+dep 'Alfred.app' do
+  source 'http://media.alfredapp.com/alfred_0.6.6_beta_34.dmg'
+end
+
+dep '1Password.app' do
+  source 'http://aws.cachefly.net/aws/dmg/1PW3/English/1Password-3.2.5.dmg'
+end
+
+dep 'HTTPScoop.app' do
+  source 'http://www.tuffcode.com/releases/HTTPScoop_1.4.3.dmg'
+end
+
