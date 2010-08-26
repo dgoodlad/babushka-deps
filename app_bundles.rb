91ed20a6e50f29138b0836c9fcb2216fd2ecd89d
David Goodlad <david@goodlad.ca>
2010-08-27 09:19:42 +1000

    Update XQuartz to 2.5.3

--- app_bundles.rb
+++ app_bundles.rb
@@ -3,7 +3,7 @@ dep 'Google Chrome.app' do
 end
 
 dep 'XQuartz.installer' do
-  source 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.5.1.dmg'
+  source 'http://xquartz.macosforge.org/downloads/SL/XQuartz-2.5.3.dmg'
   met? { '/Applications/Utilities/XQuartz.app'.p.exists?  }
 end
 
