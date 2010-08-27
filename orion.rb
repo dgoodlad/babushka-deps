c780fdb7231eac66d93420de9ce347367706ea1c
David Goodlad <david@goodlad.ca>
2010-08-27 13:29:54 +1000

    Add a dep to automatically build orion

---
+++ orion.rb
@@ -0,0 +1,26 @@
+# Complete setup for my Macbook Pro, 'orion'
+
+dep 'orion' do
+  requires 'Google Chrome.app',
+           'Echofon.app',
+           'Skype.app',
+           'Dropbox.app',
+           '1Password.app',
+           'Growl.installer',
+           'LaunchBar.app',
+           #'Alfred.app',
+           'SizeUp.app',
+           'iStat Menus.app',
+           'Airfoil.app',
+           #'iTerm.app',
+           'HTTPScoop.app',
+           'Sequel Pro.app',
+           'GitX.app',
+           'tmux'
+           #'macvim'
+
+  requires 'rvm'
+  requires 'couchdb'
+  requires 'nvm'
+end
+
