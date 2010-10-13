537793972bbb3540a12740065cc0d85aa1946590
David Goodlad <david@goodlad.ca>
2010-10-13 20:40:48 +1100

    Add a ttf meta dep, and the Meslo font

--- osx.rb
+++ osx.rb
@@ -7,3 +7,41 @@ dep 'subpixel smoothing enabled' do
     shell "defaults -currentHost write -globalDomain AppleFontSmoothing -int 2"
   }
 end
+
+dep 'user font dir exists' do
+  met? {
+    "~/Library/Fonts".p.dir?
+  }
+  meet {
+    log_shell "Creating ~/Library/Fonts", "mkdir ~/Library/Fonts"
+  }
+end
+
+meta 'ttf' do
+  accepts_list_for :source
+  accepts_list_for :extra_source
+  accepts_list_for :ttf_filename
+
+  template {
+    requires 'user font dir exists'
+    prepare {
+      setup_source_uris
+    }
+    met? {
+      "~/Library/Fonts/#{ttf_filename.first}".p.exists?
+    }
+    meet {
+      process_sources do
+        Dir.glob("*.ttf") do |font|
+          log_shell "Installing #{font}", "cp #{font} ~/Library/Fonts"
+        end
+      end
+    }
+  }
+end
+
+dep 'meslo.ttf' do
+  source "http://github.com/downloads/andreberg/Meslo-Font/Meslo%20LG%20DZ%20v1.0.zip"
+  ttf_filename "MesloLGM-DZ-Regular.ttf"
+end
+
