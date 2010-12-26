8c3be5edf23e17522774a2de0a540812bbb6a40b
David Goodlad <david@goodlad.ca>
2010-12-26 15:14:59 +1100

    Update my laptop's build dep

--- orion.rb
+++ orion.rb
@@ -1,27 +1,49 @@
 # Complete setup for my Macbook Pro, 'orion'
 
 dep 'orion' do
-  requires 'Google Chrome.app',
-           'Echofon.app',
-           'Skype.app',
-           'Dropbox.app',
-           '1Password.app',
-           'Growl.installer',
-           'LaunchBar.app',
-           #'Alfred.app',
-           'SizeUp.app',
-           'iStat Menus.app',
-           'Airfoil.app',
-           #'iTerm.app',
-           'HTTPScoop.app',
-           'Sequel Pro.app',
-           'GitX.app',
+  requires 'orion osx apps installed'
+
+  requires 'macvim',
            'tmux',
-           'VMWare Fusion.installer'
-           #'macvim'
+           'ack.managed',
+           'tree.managed'
 
   requires 'rvm'
-  requires 'couchdb'
   requires 'nvm'
 end
 
+dep 'orion osx apps installed' do
+  # Social, Web, Media etc.
+  requires 'Google Chrome.app',
+           'Echofon.app',
+           'Skype.app',
+           'LimeChat.app',
+           'Airfoil.app'
+
+  # System utilities
+  requires 'Dropbox.app',
+           'Growl.installer',
+           'iStat Menus.app',
+           'KeyRemap4MacBook.installer',
+           'Bonjour Browser.app',
+           'gfxCardStatus.app'
+
+  # Tools
+  requires 'LaunchBar.app',
+           'SizeUp.app',
+           '1Password.app',
+           'The Hit List.app',
+           'TextExpander.app'
+
+  # Development
+  requires 'iTerm.app',
+           'HTTPScoop.app',
+           'Sequel Pro.app',
+           'GitX.app',
+           'Tower.app',
+           'Kaleidoscope.app',
+           'Reggy.app',
+           'VMWare Fusion.installer',
+           'VirtualBox.installer',
+           'CoRD.app'
+end
